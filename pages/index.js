e165e239d833b5e429fd720f5ff08c137c1f480d
xiaosongread <598349146@qq.com>
2019-09-24 17:32:26 +0800

    生成国庆微信头像

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,112 @@
 var app = getApp();
 Page({
-    data: {
-    	data: [],
-		page: 1, //记录当前请求数据的页数
-		count: 0 //文章列表总共的页数
-    },
-    onReady: function () {
-		this.getData();
-	},
-	getData: function () {
-		wx.showToast({
-			title: '加载中',
-			icon: 'loading',
-			duration: 500
-		});
-		var _this = this;
-		wx.request({
-			url: app.api.domainName + '/api/contentList',
-			method: 'get',
-			data:{
-				page:_this.data.page,
-				limte:10
-			},
-			success: function (res) {
-				console.log(res.data.data)
-				res.data.data.forEach(function(item,index){
-					res.data.data[index].startTime = app.fn.setTime(res.data.data[index].startTime)
-				})
-				_this.setData({
-					count: res.data.count,
-					data: _this.data.data.concat(res.data.data)
-				})
-				wx.hideToast();
-			}
-		})
-	},
-	jumpFn: function(e){
-		var id = e.currentTarget.id;
-		wx.navigateTo({
-		  url: './contentInfo/contentInfo?id='+id
-		})
-	},
-	onReachBottom: function () {
-		this.setData({
-			page: ++ this.data.page
-		})
-		if(this.data.count != this.data.data.length){
-				this.getData();
-		}
-	},
-	// onPullDownRefresh: function () {
-	// 	wx.stopPullDownRefresh();
-	// 	this.data.page = 0;
-    //     this.getData();
-    // },
-	onShareAppMessage: function () {//设置分享
-        return {
-          title: '抿圪斗博客',
-          desc: '抿圪斗博客-首页',
-          path: 'index/index'
-        }
+  data: {
+    decorationImage: '../img/1.jpg', // 右下角装饰图片
+    canvasW: 300,
+    canvasH: 300,
+    canvasWidth: 250,
+    canvasHeight: 250,
+    smallImage: 74,
+    marginWidth: 25,
+    headerImage: ''
+  },
+  onReady: function () {
+    this.drawCanvas()
+  },
+  // 获取相册头像图片
+  setHeader() {
+    let that = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: 'original', // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: 'album', // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
+        that.setData({
+          headerImage: tempFilePaths[0]
+        });
+        that.drawCanvas()
+      }
+    })
+  },
+  // 绘制圆角矩形
+  roundRectColor: function (context, x, y, w, h, r) {  //绘制圆角矩形（纯色填充）
+    context.save();
+    context.fillStyle = "#fff";
+    context.strokeStyle = '#fff';
+    context.lineJoin = 'round';  //交点设置成圆角
+    context.lineWidth = r;
+    context.strokeRect(x + r / 2, y + r / 2, w - r, h - r);
+    context.fillRect(x + r, y + r, w - r * 2, h - r * 2);
+    context.stroke();
+    context.closePath();
+  },
+  // 渲染画布内容
+  drawCanvas: function () {
+    var that = this;
+    // 拿到canvas context
+    let ctx = wx.createCanvasContext('share_canvas');
+    this.roundRectColor(ctx, that.data.marginWidth, that.data.marginWidth, this.data.canvasWidth, this.data.canvasHeight, 30)
+    // ctx.setFillStyle('white')
+    // ctx.fillRect(0, 0, this.data.canvasWidth, this.data.canvasHeight);
+    if (that.data.headerImage) {
+      ctx.drawImage(that.data.headerImage, 35, 35, this.data.canvasWidth - 20, this.data.canvasHeight - 20)
     }
+    let x = this.data.canvasWidth - (that.data.smallImage - 20)
+    let y = this.data.canvasWidth - (that.data.smallImage - 20)
+    ctx.drawImage(this.data.decorationImage, x, y, that.data.smallImage, that.data.smallImage)
+    ctx.draw();
+  },
+  // 保存图片到本地
+  saveImage: function () {
+    wx.showLoading({
+      title: '正在保存图片..',
+    });
+    let that = this;
+    wx.canvasToTempFilePath({
+      x: that.data.marginWidth,
+      y: that.data.marginWidth,
+      fileType: 'jpg',
+      width: that.data.canvasW - (that.data.marginWidth * 2),
+      height: that.data.canvasH - (that.data.marginWidth * 2),
+      destWidth: 960,
+      destHeight: 960,
+      canvasId: 'share_canvas',
+      success: function (res) {
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success(res) {
+            console.log(res);
+            wx.showToast({
+              title: '保存到相册成功',
+              duration: 1500,
+            })
+          },
+          fail(res) {
+            console.log(res)
+            wx.showToast({
+              title: '保存到相册失败',
+              icon: 'fail'
+            })
+          },
+          complete(res) {
+            console.log(res)
+          }
+        })
+      }
+    })
+  },
+  getData: function () {
+
+  },
+  //设置分享
+  onShareAppMessage: function () {
+    return {
+      title: '',
+      desc: '',
+      path: ''
+    }
+  }
 })
